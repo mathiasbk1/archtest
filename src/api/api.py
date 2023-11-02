ec04414886d43c299b7891c6a928dc86392efc85
mathiasbk1 <mathias.b.k@hotmail.com>
2023-11-02 22:05:36 +0100

    test

---
+++ src/api/api.py
@@ -0,0 +1,10 @@
+from tp_src.tp_core.core import add
+
+from tp_src.api.sub_api import sub_api
+
+
+
+
+
+def get(x, y):
+    return add(x, y)
---
+++ src/tp_core/core.py
@@ -0,0 +1,7 @@
+from tp_src.controller.controller import save
+from tp_src.tp_core.sub_core import return_meaning_of_life
+
+def add(x, y):
+    save(x)
+    save(y)
+    return x + y
