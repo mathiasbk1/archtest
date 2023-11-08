fc5b52357856013641481fe5b8df5ae96ef6894f
Mathias Baunsgaard Kristensen <mak@novataris.com>
2023-11-08 21:50:15 +0100

    test new stuff

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,5 +1,5 @@
 # from src.api.sub_api.api_test import sub_api_test
-# from src.api.sub_api.api_logic import sub_api_logic
+from src.api.sub_api.api_logic import sub_api_logic
 
 def save():
     print("xx")
